cc736cefcc5a24e491d735e5ed14fcbc11532c55
Jijo Thomas <jijo487@gmail.com>
2015-11-27 16:27:45 +0530

    Overriden callback_phase method to hack for the front-end JS

--- discourse-marlabs-ldap-loginUI/lib/omniauth/stratergies/ldap.rb
+++ discourse-marlabs-ldap-loginUI/lib/omniauth/stratergies/ldap.rb
@@ -16,6 +16,16 @@ module OmniAuth
         f.button "Sign In", "ldap-submit", "f-submit"
         f.to_response
       end
+
+      def callback_phase
+        @adaptor = OmniAuth::LDAP::Adaptor.new @options
+        fail "Missing Credentials" if missing_credentials? #Hack for the JS
+        @ldap_user_info = @adaptor.bind_as(:filter => Net::LDAP::Filter.eq(@adaptor.uid, @options[:name_proc].call(request['username'])),:size => 1, :password => request['password'])
+        fail "Invalid Credentials" if !@ldap_user_info #Hack for the JS
+        @user_info = self.class.map_user(@@config, @ldap_user_info)
+        super
+      end
+
     end
   end
 end
